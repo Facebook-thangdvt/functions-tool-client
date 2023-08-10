fb10af15f3a0b52791c07e5a17434cfb4c8e2358
thangsm <releaseth3beast@gmail.com>
2023-08-10 23:43:44 +0700

    first init branch, improve development enviroment

--- src/features/login.ts
+++ src/features/login.ts
@@ -1,21 +1,23 @@
-import type { WebDriver } from "selenium-webdriver"
-import { By } from "selenium-webdriver"
-import { LoginBtnXpath } from "../xpaths"
+import type { WebDriver } from "selenium-webdriver";
+import { By } from "selenium-webdriver";
+import { LoginBtnXpath } from "../xpaths";
 
-type LoginInput = { email: string, password: string }
+type LoginInput = { email: string; password: string };
 
-export default async function login(driver: WebDriver, { email, password }: LoginInput) {
-    try {
-        await driver.findElement(By.id("email")).sendKeys(email)
-        await driver.findElement(By.id("pass")).sendKeys(password)
+export default async function login(
+	driver: WebDriver,
+	{ email, password }: LoginInput
+) {
+	try {
+		await driver.findElement(By.id("email")).sendKeys(email);
+		await driver.findElement(By.id("pass")).sendKeys(password);
 
-        await driver.findElement(By.xpath(LoginBtnXpath)).click()
+		await driver.findElement(By.xpath(LoginBtnXpath)).click();
 
-        let cookies = await driver.manage().getCookies()
+		// let cookies = await driver.manage().getCookies();
 
-        // save cookie to db
-
-    } catch (error) {
-        console.log(error)
-    }
+		// save cookie to db
+	} catch (error) {
+		console.log(error);
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,22 @@
-import { Builder, Browser, By, Key, until } from "selenium-webdriver"
-import { io } from "socket.io-client"
+import { Builder, Browser, By } from "selenium-webdriver";
+import { io } from "socket.io-client";
 
-import accounts from "./dummy/accounts"
-import { login } from "./features"
-import { LikeBtnXpath } from "./xpaths"
+import accounts from "./dummy/accounts";
+import { login } from "./features";
+import { LikeBtnXpath } from "./xpaths";
 
-async function facebook(user: {
-    email: string,
-    password: string
-}) {
-    const driver = await new Builder().forBrowser(Browser.FIREFOX).build()
-    try {
+async function facebook(user: { email: string; password: string }) {
+	const driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+	try {
+		let socket = io("http://localhost:7777");
 
-        let socket = io("http://localhost:7777")
+		await driver.get(`https://facebook.com`);
 
-        await driver.get(`https://facebook.com`)
+		await login(driver, { email: user.email, password: user.password });
 
-        await login(driver, { email: user.email, password: user.password })
+		// trong truong hop can thiet phai su dung den cookie: https://viblo.asia/p/tao-mot-tro-ly-ao-tren-facebook-tu-dong-gui-tin-tuc-moi-nhat-hang-ngay-bang-python-selenium-eW65G1L9ZDO
 
-        // trong truong hop can thiet phai su dung den cookie: https://viblo.asia/p/tao-mot-tro-ly-ao-tren-facebook-tu-dong-gui-tin-tuc-moi-nhat-hang-ngay-bang-python-selenium-eW65G1L9ZDO
-
-        /*
+		/*
             - Xu ly event voi socket (client)
             - Vi du cac event nhu like, follow cho 1 user nao do
 
@@ -29,29 +25,27 @@ async function facebook(user: {
                 // xu ly like, follow, ...
             })
         */
+		socket.on("like", async (data) => {
+			try {
+				await driver.get(`https://facebook.com/${data.to}`);
+				await driver.sleep(3000);
+				await driver.findElement(By.xpath(LikeBtnXpath)).click();
 
-        socket.on("like", async (data) => {
-            try {
-                await driver.get(`https://facebook.com/${data.to}`)
-                await driver.sleep(3000)
-                await driver.findElement(By.xpath(LikeBtnXpath)).click()
+				socket.emit("liked", {
+					msg: `${user.email} liked to ${data.to}`,
+					data: { action: "like", email: user.email, to: data.to },
+				});
 
-                socket.emit("liked", {
-                    msg: `${user.email} liked to ${data.to}`,
-                    data: { action: 'like', email: user.email, to: data.to }
-                })
-
-                // Se phai xu ly loi neu user die
-            } catch (err) {
-                console.log(`already liked`)
-            }
-
-        })
-    } catch (err) {
-        console.log(err)
-        await driver.quit()
-    }
+				// Se phai xu ly loi neu user die
+			} catch (err) {
+				console.log(`already liked`);
+			}
+		});
+	} catch (err) {
+		console.log(err);
+		await driver.quit();
+	}
 }
-const handers = accounts.map(i => facebook(i))
+const handers = accounts.map((i) => facebook(i));
 
-Promise.all(handers)
+Promise.all(handers);
